73669cab1d6b6de168a30b865052df92455ce87a
Dmitry S. <dimah1996@mail.ru>
2026-06-12 16:54:01 +0300

    Sprint9. header, modal, success classes

---
+++ src/components/views/Header.ts
@@ -0,0 +1,23 @@
+import { ensureElement } from "../../utils/utils";
+import { Component } from "../base/Component";
+import { IEvents } from "../base/Events";
+
+interface IHeader {
+  counter: number;
+}
+
+export class Header extends Component<IHeader> {
+  protected basketButton: HTMLButtonElement;
+  protected counterElement: HTMLElement;
+
+  constructor(container: HTMLElement, protected events: IEvents) {
+    super(container);
+
+    this.basketButton = ensureElement<HTMLButtonElement>('.header__basket', this.container);
+    this.counterElement = ensureElement<HTMLElement>('.header__basket-counter', this.container);
+
+    this.basketButton.addEventListener('click', () => {
+      this.events.emit('shopping-cart:open');
+    });
+  }
+}
